ea12cf8fcf43239c54198c19144b517195c06937
Jason Feinstein <jason@bandcamp.com>
2016-11-11 14:31:27 +0100

    Add a test case for dirty params. Use Html.escapeHtml for dirty params passed to getString, as you'd do.

--- library/src/androidTest/java/co/jasonwyatt/srml/PerformanceTest.java
+++ library/src/androidTest/java/co/jasonwyatt/srml/PerformanceTest.java
@@ -1,6 +1,8 @@
 package co.jasonwyatt.srml;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.text.Html;
@@ -60,6 +62,31 @@ public class PerformanceTest {
         startTime = System.currentTimeMillis();
         for (int i = 0; i < iterations; i++) {
             Html.fromHtml(context.getString(R.string.simple_html_string_with_param, Integer.toString(i)));
+        }
+        long elapsedHTML = System.currentTimeMillis() - startTime;
+
+        double srmlTimePer = elapsedSRML / (double) iterations;
+        double htmlTimePer = elapsedHTML / (double) iterations;
+        Log.i(TAG, String.format(Locale.US, "SRML.getString averaged %.2fms", srmlTimePer));
+        Log.i(TAG, String.format(Locale.US, "Html.fromHtml averaged %.2fms", htmlTimePer));
+        assertTrue("SRML.getString should be as fast, or faster than, Html.fromHtml.", srmlTimePer <= htmlTimePer);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Test
+    public void testSRMLvsHTMLLegacyWithDirtyParam() {
+        int iterations = 100000;
+        Context context = InstrumentationRegistry.getContext();
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+            SRML.getString(context, R.string.simple_srml_string_with_param, String.format(Locale.US, "{{b}}%d{{/b}}", i));
+        }
+        long elapsedSRML = System.currentTimeMillis() - startTime;
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+            Html.fromHtml(context.getString(R.string.simple_html_string_with_param, Html.escapeHtml(String.format(Locale.US, "<b>%d</b>", i))));
         }
         long elapsedHTML = System.currentTimeMillis() - startTime;
 
